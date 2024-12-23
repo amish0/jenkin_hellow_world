423e8c8e856b92057383ff97a060e560ac7508ff
amish0 <amishkumar562@gmail.com>
2024-12-23 12:37:24 +0530

    function added

--- hellow_world.c
+++ hellow_world.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
 
+int add_number(int a, int b)
+{
+    return a+b;
+}
+
 int main()
 {
     printf("\nHellow World & Welcome to Jenkin Training\n");
+    int a=10;
+    int b=20;
+    int c;
+    c = add_number(a,b);
+    printf("\naddition of two number %d and %d is %d", a, b, c);
     return 0;
 }
