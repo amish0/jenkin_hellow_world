1f6dd431e7aa4b74e396040b6f67a959cfe330e3
amish0 <amishkumar562@gmail.com>
2024-12-23 18:22:16 +0530

    add subtraction function

--- hellow_world.c
+++ hellow_world.c
@@ -5,14 +5,25 @@ int add_number(int a, int b)
     return a+b;
 }
 
+int sub_number(int a, int b)
+{
+    return a-b;
+}
+
 int main()
 {
     printf("\nHellow World & Welcome to Jenkin Training\n");
     int a=10;
     int b=20;
     int c;
+    int d;
     printf("test a the addition of two number");
     c = add_number(a,b);
     printf("\naddition of two number %d and %d is %d", a, b, c);
+    d = sub_number(a,b);
+    printf("\nsubtraction of two number %d and %d is %d", a, b, d);
+    printf("\n\n");
+    printf("<--------------BYE-------------->");
+    printf("See you again.....");
     return 0;
 }
